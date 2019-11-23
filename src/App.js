7a4d3704ea85c2b5ecac35b8f9f90135969cb96f
ajimenez <anthonyjt21>
2019-11-22 20:34:04 -0600

    react rotuer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import AppBar from '@material-ui/core/AppBar';
+import { Toolbar, Typography } from '@material-ui/core';
+import { BrowserRouter as Router, Route ,Switch } from 'react-router-dom';
+import Navbar from  './components/Navbar';
+import Post from './components/Posts';
+import Contact from './components/Contact';
+//import ListUserComponent from "./user/ListUserComponent";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+      <Router >
+      <Switch>
+        <Route path="/" component={Contact} />
+        <Route path="/Contact" component={Contact}/>
+        </Switch>
+      </Router>
+     <AppBar color="primary" position="static">
+       <Toolbar>
+         <Typography variant="title"
+          color="inherit">
+       <Navbar></Navbar>
+       </Typography>
+       </Toolbar>
+     </AppBar>
+   </div>
   );
 }
 
